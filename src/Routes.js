05afd0db2a26a83b80b49f0c374de2bd6880387f
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-21 12:34:22 -0600

    rutas privadas

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Home from './components/home/HomePage'
-import FavPage from './components/favs/FavPage'
-import LoginPage from './components/login/LoginPage'
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default function Routes() {
-    return (
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/favs" component={FavPage} />
-            <Route path="/login" component={LoginPage} />
-        </Switch>
-    )
+import Home from "./components/home/HomePage";
+import FavPage from "./components/favs/FavPage";
+import LoginPage from "./components/login/LoginPage";
+
+function PrivateRoute({ loggedIn, path, component, ...rest }) {
+  //   let storage = localStorage.getItem("storage");
+  //   storage = JSON.parse(storage);
+  //   if (storage && storage.user) {
+  //     return <Route path={path} component={component} />;
+  //   } else {
+  //     return <Redirect to="/login" {...rest} />;
+  //   }
+  if (loggedIn) {
+    return <Route path={path} component={component} />;
+  } else {
+    return <Redirect to="/login" {...rest} />;
+  }
 }
+
+function Routes({ loggedIn }) {
+  return (
+    <Switch>
+      <PrivateRoute loggedIn={loggedIn} exact path="/" component={Home} />
+      <PrivateRoute loggedIn={loggedIn} path="/favs" component={FavPage} />
+      <Route path="/login" component={LoginPage} />
+    </Switch>
+  );
+}
+
+function mapState({ user: { loggedIn } }) {
+  return { loggedIn };
+}
+
+export default connect(mapState)(Routes);
